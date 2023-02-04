af2dccce0a2fa7ec59b7c8cf141289dd66c292e1
beetrbgus <devbeetrb0214@gmail.com>
2023-02-04 13:17:24 +0900

    initial commit 추가
    - 프로젝트가 다른 환경에서도 실행가능하게 버전관리 안되는 파일들을 추가

---
+++ 황규현/firstWeek/src/main/java/com/devbeetrb/firstweek/FirstWeekApplication.java
@@ -0,0 +1,13 @@
+package com.devbeetrb.firstweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstWeekApplication.class, args);
+    }
+
+}
---
+++ 황규현/firstWeek/src/test/java/com/devbeetrb/firstweek/FirstWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbeetrb.firstweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
