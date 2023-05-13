aa3364f0b5db4024b03e33f895f564ca04ac1350
hyojeong <khj6165@gmail.com>
2023-05-13 15:41:50 +0900

    1주차 과제

---
+++ 김효정/first-study/first-study/src/test/java/first/firststudy/FirstStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package first.firststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
